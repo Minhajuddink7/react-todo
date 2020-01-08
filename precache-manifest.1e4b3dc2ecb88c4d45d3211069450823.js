f71888e9426308b19a24b6ebe63495b6f49fe3f3
Minhajuddink7 <Minhajuddink7@gmail.com>
2020-01-08 20:31:47 +0530

    Updates

---
+++ precache-manifest.1e4b3dc2ecb88c4d45d3211069450823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21d7749620c80921cf9492da3218d9d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "6328e2ee7c0a1c47e0b7",
+    "url": "/react-todo/static/css/main.2c2a1706.chunk.css"
+  },
+  {
+    "revision": "3f0ec15ec5c3a1dda583",
+    "url": "/react-todo/static/js/2.7211b22f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.7211b22f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6328e2ee7c0a1c47e0b7",
+    "url": "/react-todo/static/js/main.8be093ce.chunk.js"
+  },
+  {
+    "revision": "ec66a91b23915f88916c",
+    "url": "/react-todo/static/js/runtime-main.b72df489.js"
+  }
+]);
